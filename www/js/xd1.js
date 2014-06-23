92ab7aed097ccf2aa8f0e4cab809c5393b0aa772
Nunki <sprimont@iasfbo.inaf.it>
2014-06-23 16:52:57 +0200

    ...

--- www/js/xd1.js
+++ www/js/xd1.js
@@ -336,7 +336,7 @@ window.onload = function(){
     var hostname//="ws://192.168.1.134:9999";
 	="ws://localhost:9999";
     
-    sadira=new sadira({ server : hostname, widget_prefix : "widgets", server_prefix : "~fullmoon/XD-1"}, function(error){
+    sadira=new sadira({ server : hostname, widget_prefix : "widgets", server_prefix : "~fullmoon/XD-1/www"}, function(error){
 	console.log("Error sadira init : " + JSON.stringify(error));
 	xd1= new xdone();
 	
